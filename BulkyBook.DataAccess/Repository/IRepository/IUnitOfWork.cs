ea4fc8756331e12186c861672937dd37cd40c1cd
idani <idaninho@gmail.com>
2021-09-11 14:03:26 +0300

    Add project files.

---
+++ BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ICategoryRepository Category { get; }
+        ICoverTypeRepository CoverType { get; }
+        ICompanyRepository Company { get; }
+
+        IApplicationUserRepository ApplicationUser { get; }
+        IProductRepository Product { get; }
+
+
+        IShoppingCartRepository ShoppingCart { get; }
+        IOrderDetailsRepository OrderDetails { get; }
+        IOrderHeaderRepository OrderHeader { get; }
+        ISP_Call SP_Call { get; }
+
+        void Save();
+    }
+}
